b90c1387486e9f17ff68c2ce8a45b79a5709bc28
Makoto Huang <cingyo316@gmail.com>
2024-08-07 17:59:07 +0800

    .

---
+++ rent_car/src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import Login from '../components/Login';
+
+const LoginPage = () => {
+    return <Login />;
+};
+
+export default LoginPage;
