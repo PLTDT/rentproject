946c873e15df24afe0cb351edaf755d6540159c9
Howard <iamxyzgos5933@gmail.com>
2024-09-03 16:42:29 +0800

    .

--- rent_car/src/index.js
+++ rent_car/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
